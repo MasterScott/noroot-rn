4e9a9c9cdb20e5904a60f6937cdcf37be1577fba
Vinícius <vrm@cin.ufpe.br>
2018-04-17 20:03:19 -0300

    Update rn.sh

--- rn.sh
+++ rn.sh
@@ -1,45 +1,15 @@
 #!/bin/bash
 #https://facebook.github.io/react-native/docs/getting-started.html
-site_node="https://nodejs.org"
 tmp="$(mktemp -d)"
-so="$(uname | tr '[:upper:]' '[:lower:]')"
 
-if [ ! "$so" = "linux" ] && [ ! "$so" = "darwin" ]; then
-    echo "Desculpe, sistema não suportado (no momento apenas Linux e macOS)."
-    exit 0
+if [ "$(id -u)" = "0" ];then
+    echo "Não é recomendado executar esse script como root. Tente rodá-lo com um usuário comum ou comente a linha indicada no script se quiser forçar isso."
+    exit 0 #essa linha
 fi
 
-echo "Verificando versão mais recente disponível do node."
-a=$(curl -sL "$site_node/en/download/")
-
-version="v"$(echo $a | sed -e 's/.*Version: <strong>\([^<]*\)<.*/\1/')
-echo 'Encontrado node '$version' para download.'
-
-
-if [ "$FORCE_NODE" = "1" ] || [ -z "$(which npm 2>/dev/null)" ] || (( $(node --version | sed "s/v\(.\).*/\1/") < ${version:1:1} )); then
-    echo "Baixando ..."
-    cd "${tmp:?}"
-    link="$site_node/dist/$version/node-$version-"$so"-x64.tar.xz"
-    curl -L -# "$link" -o node.tar.xz
-    tar -Jxf node.tar.xz
-    rm -rf "${HOME:?}"/.node_noroot "${HOME:?}"/.npm
-    mv "${tmp:?}"/node*/ "$HOME"/.node_noroot
-    rm "${tmp:?}/node.tar.xz"
-    echo 'export PATH='$HOME'/.node_noroot/bin/:"$PATH"' > $HOME/.noderc
-    echo 'source '$HOME'/.noderc' >> $HOME/.bashrc
-    source $HOME/.noderc
-else
-    echo 'Versão local já está atualizada.'
-fi
-
-echo -en '\n'
-
-if [ "$so" = "darwin" ]; then #configuracao adicional para mac
-    touch $HOME/.bash_profile
-    if ( ! grep -q [.]bashrc "$HOME/.bash_profile" );then
-        echo 'source '$HOME'/.bashrc' >> $HOME/.bash_profile
-    fi
-fi
+cd "${tmp:?}"
+git clone https://github.com/wison27/noroot-node && cd noroot-node && bash node.sh
+rm -rf "${tmp:?}"/noroot-node
 
 if [ -z "$(which react-native 2>/dev/null)" ]; then
     echo 'Instalando React Native.'
