282b750076a9b51dc4ad9bf865852d10c4196055
WisON27 <vrm@cin.ufpe.br>
2018-04-17 20:52:16 -0300

    fix problems & update msg

--- rn.sh
+++ rn.sh
@@ -7,21 +7,21 @@ if [ "$(id -u)" = "0" ];then
     exit 0 #essa linha
 fi
 
-cd "${tmp:?}"
-git clone https://github.com/wison27/noroot-node && cd noroot-node && bash node.sh
-rm -rf "${tmp:?}"/noroot-node
-source $HOME/.noderc
+curl -L 'https://raw.githubusercontent.com/wison27/noroot-node/master/node.sh' | bash
+source $HOME/.norootrc
 
-if [ -z "$(which react-native 2>/dev/null)" ]; then
-    echo 'Instalando React Native.'
+if [ -z "$(which react-native 2>/dev/null)" ] || [ -z "$(which create-react-native-app 2>/dev/null)" ]; then
+    echo 'Instalando react-native-cli e create-react-native-app'
     own=$(ls -lah $(which node) | awk '{print $3}')
     opt=''
     if [ ! "$own" = "$(whoami)" ]; then
         echo 'O node está instalado no diretório raiz, por isso é preciso de acesso root apenas para instalar o módulo ou execute nesse terminal: "export FORCE_NODE=1" e rode esse script novamente.'
         opt='sudo'
     fi
-    eval "$opt npm install -g react-native-cli"
+    eval "$opt npm install -g react-native-cli create-react-native-app"
 fi
+
+so="$(uname | tr '[:upper:]' '[:lower:]')"
 
 if [ "$so" = "linux" ] && [ -z "$ANDROID_HOME" ]; then
     echo "Instalando Android SDK"
@@ -30,5 +30,5 @@ if [ "$so" = "linux" ] && [ -z "$ANDROID_HOME" ]; then
     cd "${tmp:?}"
     rm -rf noroot-android
 fi
-echo "Tudo pronto!"
+echo -e "React-Native Ok!\nTalvez seja necessário reiniciar os terminais em execução para que as alterações sejam detectadas."
 exec -l bash
